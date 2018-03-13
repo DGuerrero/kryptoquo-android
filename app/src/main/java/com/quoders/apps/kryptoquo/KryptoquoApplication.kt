a1573402c602842c74b23d43c217f332ea2af396
David Guerrero <david.guerrero@quoders.com>
2018-03-13 00:40:18 +0000

    [WIP] Adding Room

---
+++ app/src/main/java/com/quoders/apps/kryptoquo/KryptoquoApplication.kt
@@ -0,0 +1,15 @@
+package com.quoders.apps.kryptoquo
+
+import android.app.Application
+import android.arch.persistence.room.Room
+import com.quoders.apps.kryptoquo.data.model.db.AppDatabase
+
+class KryptoquoApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        val appDatabase = Room.databaseBuilder(applicationContext,
+                AppDatabase::class.java, "database-name").build()
+    }
+}
---
+++ data/src/androidTest/java/com/quoders/apps/kryptoquo/data/AssetEntityReadWriteTest.kt
@@ -0,0 +1,44 @@
+package com.quoders.apps.kryptoquo.data
+
+import android.arch.persistence.room.Room
+import android.content.Context
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+import com.quoders.apps.kryptoquo.data.model.db.AppDatabase
+import com.quoders.apps.kryptoquo.data.model.db.Asset
+import com.quoders.apps.kryptoquo.data.model.db.AssetDao
+import junit.framework.Assert.assertTrue
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class AssetEntityReadWriteTest {
+
+    lateinit var appDatabase: AppDatabase
+    lateinit var assetDao: AssetDao
+
+    @Before
+    fun setup() {
+        val context: Context = InstrumentationRegistry.getTargetContext()
+        appDatabase = Room.inMemoryDatabaseBuilder(context, AppDatabase::class.java).build()
+        assetDao = appDatabase.assetDao()
+    }
+
+    @After
+    fun closeDb() = appDatabase.close()
+
+    @Test
+    fun writeAssetAndReadInList() {
+        var asset = Asset()
+        asset.assetId = "ZCG"
+        asset.name = "Zlancer"
+        asset.dateStart = "2018-01-24"
+
+        assetDao.insertAll(asset)
+        val allAssets = assetDao.getAll()
+
+        assertTrue(allAssets.size == 1)
+    }
+}
